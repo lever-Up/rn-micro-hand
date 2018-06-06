c56d760f91c7d6de5a02ca64bb8f973445b2f08c
ganyangxing <172176234@qq.com>
2018-06-07 01:13:27 +0800

    Init Commit

---
+++ app/index.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { AppRegistry, YellowBox } from 'react-native'
+
+import dva from './utils/dva'
+import Router, { routerMiddleware } from './router'
+
+import appModel from './models/app'
+import routerModel from './models/router'
+
+YellowBox.ignoreWarnings(['Warning: isMounted(...) is deprecated', 'Module RCTImageLoader'])
+
+const app = dva({
+  initialState: {},
+  models: [appModel, routerModel],
+  onAction: [routerMiddleware],
+  onError(e) {
+    console.log('onError', e)
+  },
+})
+
+const App = app.start(<Router />)
+
+AppRegistry.registerComponent('DvaStarter', () => App)
